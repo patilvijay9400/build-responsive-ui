d9ee5c1da64876276a4cfbde66bdce927f554cdc
patilvijay9400 <patilvijay416940@gmail.com>
2023-08-18 09:47:16 +0530

    some fuctionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./App.css";
 import BannerSlider from "./components/BannerSlider";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
+import Tab from "./components/Tab";
 
 function App() {
   const images = [
@@ -9,6 +12,29 @@ function App() {
     "image3.jpg",
     // Add more image URLs here
   ].map((image) => process.env.PUBLIC_URL + image);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+
+  const openModal = (content) => {
+    setModalOpen(true);
+    setModalContent(content);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setModalContent("");
+  };
+
+  const [activeTab, setActiveTab] = useState("Form");
+
+  const tabContent = {
+    Form: <div>Form Content</div>,
+    Text: <div>Text Content</div>,
+    Images: <div>Images Content</div>,
+    Video: <div>Video Content</div>,
+  };
+
   return (
     <>
       <Header />
@@ -39,6 +65,38 @@ function App() {
           </div>
         </div>
       </div>
+
+      <button onClick={() => openModal("Tab 1 Content")}>Open Tab 1</button>
+      <button onClick={() => openModal("Tab 2 Content")}>Open Tab 2</button>
+
+      <Modal isOpen={modalOpen} onClose={closeModal}>
+        {modalContent}
+      </Modal>
+
+      <div className="tab-nav">
+        <Tab
+          label="Form"
+          isActive={activeTab === "Form"}
+          onClick={() => setActiveTab("Form")}
+        />
+        <Tab
+          label="Text"
+          isActive={activeTab === "Text"}
+          onClick={() => setActiveTab("Text")}
+        />
+        <Tab
+          label="Images"
+          isActive={activeTab === "Images"}
+          onClick={() => setActiveTab("Images")}
+        />
+        <Tab
+          label="Video"
+          isActive={activeTab === "Video"}
+          onClick={() => setActiveTab("Video")}
+        />
+      </div>
+
+      <div className="tab-content">{tabContent[activeTab]}</div>
     </>
   );
 }
